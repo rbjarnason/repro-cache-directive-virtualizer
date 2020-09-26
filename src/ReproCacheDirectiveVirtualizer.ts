c3aaad45b1658d6df8975c42a12cce89310b1bcf
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-09-26 14:12:11 +0000

    test

--- src/ReproCacheDirectiveVirtualizer.ts
+++ src/ReproCacheDirectiveVirtualizer.ts
@@ -13,25 +13,6 @@ export class ReproCacheDirectiveVirtualizer extends LitElement {
   @property({ type: Array })
   itemsTwo: Array<string> | undefined;
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-    }
-
-    main {
-      flex-grow: 1;
-    }
-  `;
-
   renderItem(item: any) {
     return html`
       <div>
